6015e8c8c117e53c94601bb33fd784f20881c2f4
apetchesi <petchesi.alexandru@fmkt.ro>
2017-05-25 15:17:35 +0300

    Program.cs file updates

--- Program.cs
+++ Program.cs
@@ -10,6 +10,24 @@ namespace ObserverPattern3
     {
         static void Main(string[] args)
         {
+
+            // Create a speedometer...
+            Speedometer speedo = new Speedometer();
+
+            // Create a monitor...
+            SpeedMonitor monitor = new SpeedMonitor(speedo);
+
+            // Add automatic gearbox as an observer
+            AutomaticGearbox auto = new AutomaticGearbox(speedo);
+
+            // Drive at different speeds...
+            speedo.CurrentSpeed = 50;
+            speedo.CurrentSpeed = 70;
+            speedo.CurrentSpeed = 40;
+            speedo.CurrentSpeed = 100;
+            speedo.CurrentSpeed = 69;
+
+            Console.Read();
         }
     }
 }
